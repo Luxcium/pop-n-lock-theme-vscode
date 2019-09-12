f7da4ce04d8bd69d7d9e0341cd3e37ae57f9b93f
Benjamin Vincent <luxcium@neb401.com>
2019-09-12 15:58:10 -0400

    changes including example code
    Signed-off-by: Benjamin Vincent <luxcium@neb401.com>

--- src/resources/extension.ts
+++ src/resources/extension.ts
@@ -1,11 +1,12 @@
 'use strict';
 
-import { ExtensionContext, workspace } from 'vscode';
-import { registered } from './commands';
-import { detectConfigChanges } from './helpers/changeDetection';
-import { checkThemeStatus } from './helpers/versioning';
-import { initTranslations } from './i18n';
-import { showStartMessages } from './messages/start';
+import { ExtensionContext /* , workspace  */, window } from 'vscode';
+import * as i18n from './i18n';
+// import { registered } from './commands';
+// import { detectConfigChanges } from './helpers/changeDetection';
+// import { checkThemeStatus } from './helpers/versioning';
+// import { initTranslations } from './i18n';
+// import { showStartMessages } from './messages/start';
 
 // vscode.workspace.onDidChangeConfiguration
 
@@ -15,22 +16,33 @@ import { showStartMessages } from './messages/start';
  */
 export const activate = async (context: ExtensionContext) => {
   try {
-    await initTranslations();
-    const status = await checkThemeStatus(context.globalState);
-    showStartMessages(status);
+    window
+      .showInformationMessage(
+        // i18n.translate('themeInstalled'),
+        'THE MESSAGE TO SHOW: Show an information message to users. Optionally provide an array of items which will be presented as clickable buttons.',
+        // show 'Activate' button if icon theme is not active
+        // versioning.checkVersionSupport('1.10.0') && helpers.isThemeNotVisible()
+        /* ? */
+        i18n.translate('activate'),
+        /* : */ i18n.translate('howToActivate')
 
-    // Subscribe to the extension commands
-    context.subscriptions.push(...registered);
-
-    // Initially trigger the config change detection
-    detectConfigChanges();
-
-    // Observe changes in the config (vscode.workspace.onDidChangeConfiguration)
-    const { onDidChangeConfiguration } = workspace;
-    onDidChangeConfiguration(detectConfigChanges);
+        // i18n.translate('neverShowAgain')
+      )
+      .then(console.log /* handleWelcomeMessageActions */);
+    //   await initTranslations();
+    //   const status = await checkThemeStatus(context.globalState);
+    //   showStartMessages(status);
+    //   // Subscribe to the extension commands
+    //   context.subscriptions.push(...registered);
+    //   // Initially trigger the config change detection
+    //   detectConfigChanges();
+    //   // Observe changes in the config (vscode.workspace.onDidChangeConfiguration)
+    //   const { onDidChangeConfiguration } = workspace;
+    //   onDidChangeConfiguration(detectConfigChanges);
   } catch (error) {
     console.error(error);
   }
+  return context;
 };
 
 /** This method is called when the extension is deactivated */
--- src/resources/messages/welcome.ts
+++ src/resources/messages/welcome.ts
@@ -2,31 +2,35 @@ import open from 'open';
 import * as vscode from 'vscode';
 import { activateIcons } from '../commands/activate';
 import * as helpers from './../helpers';
-import * as versioning from './../helpers/versioning';
+// import * as versioning from './../helpers/versioning';
 import * as i18n from './../i18n';
 
-/** Show the welcome message if the icon theme has been installed the first time. */
+/**
+ * Show the welcome message if the icon theme has
+ *  been installed the first time.
+ */
 export const showWelcomeMessage = () => {
   // if the user does not want to see the welcome message
-  if (helpers.getThemeConfig('showWelcomeMessage').globalValue === false)
-    return;
+  // if (helpers.getThemeConfig('showWelcomeMessage').globalValue === false) {
+  //   return;
+  // }
 
   vscode.window
     .showInformationMessage(
-      i18n.translate('themeInstalled'),
+      // i18n.translate('themeInstalled'),
 
       // show 'Activate' button if icon theme is not active
-      versioning.checkVersionSupport('1.10.0') && helpers.isThemeNotVisible()
-        ? i18n.translate('activate')
-        : i18n.translate('howToActivate'),
+      // versioning.checkVersionSupport('1.10.0') && helpers.isThemeNotVisible()
+      /* ? */ i18n.translate('activate')
+      // /* : */ i18n.translate('howToActivate'),
 
-      i18n.translate('neverShowAgain')
+      // i18n.translate('neverShowAgain')
     )
     .then(handleWelcomeMessageActions);
 };
 
 /** Handle the actions of the welcome message. */
-const handleWelcomeMessageActions = value => {
+const handleWelcomeMessageActions = (value: any) => {
   switch (value) {
     case i18n.translate('activate'):
       activateIcons();
