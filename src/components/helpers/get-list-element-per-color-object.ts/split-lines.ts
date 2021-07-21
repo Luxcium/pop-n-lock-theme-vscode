66e905f1e521a86b97d624eaf5197ac74872a8c1
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-07-21 02:22:26 -0400

    define reverItems with the help of @MichealPearce
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/components/helpers/get-list-element-per-color-object.ts/split-lines.ts
+++ src/components/helpers/get-list-element-per-color-object.ts/split-lines.ts
@@ -1,7 +1,55 @@
+const re = /^\s*[{}[\]]$|^\s$/;
+void re;
 export async function splitLines(
   lines: Promise<string[]>
 ): Promise<[string, string][]> {
-  return (await lines).map(line => {
-    return line.split(':').reverse() as [string, string];
+  return (await lines)
+    .filter(line => line)
+    .filter(line => !re.test(line))
+    .map(line => {
+      return line.split(':').reverse() as [string, string];
+    })
+    .filter(tuple => tuple[0].trim())
+    .filter(tuple => tuple[1].trim());
+}
+// export type ArrayOrTuple<T> = Array<T> | [T, T];
+
+export async function reverseItems<T1, T2>(
+  tuples: Promise<[T1, T2][]>
+): Promise<[T2, T1][]> {
+  return (await tuples).map(tuple => tuple.reverse() as [T2, T1]);
+}
+
+export const reversedSplitLines = async (lines: Promise<string[]>) =>
+  reverseItems(splitLines(lines));
+
+// const stringTuple = ['bob', 'alice'];
+// const stringTupleAnotated: [string, string] = ['bob', 'alice'];
+
+// let stringArrayAnotated: Array<string>;
+// stringArrayAnotated = stringTupleAnotated;
+// stringArrayAnotated = stringTuple;
+
+// let tupleAnotated: [string, string];
+// tupleAnotated = stringArrayAnotated;
+// tupleAnotated = stringTuple;
+
+// void stringTuple, tupleAnotated;
+
+// export type ArrayOrTuple2<T, U> = U extends [T, T] ? [T, T] : Array<T>;
+
+export type InTuple = [string, string];
+export type OutTuple = [string, string];
+
+export function reverseItems2<I extends InTuple, O extends OutTuple>(
+  tuples: Promise<I[]>
+): Promise<O[]> {
+  return tuples.then(sets => {
+    return sets.map(set => set.reverse() as O);
   });
 }
+
+const input = Promise.resolve([['Foo and Bar', 42] as [string, number]]);
+const output = reverseItems(input);
+
+void output;
