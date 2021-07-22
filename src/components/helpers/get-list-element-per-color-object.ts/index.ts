8ae463825fddf46a221ce4b765d32ec2c33d9fd9
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-07-22 07:36:37 -0400

    refactor project structure
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- src/components/helpers/get-list-element-per-color-object.ts/index.ts
+++ src/components/helpers/get-list-element-per-color-object.ts/index.ts
@@ -4,7 +4,7 @@ import { extractColorInformation } from './extract-color-information';
 import { listElementPerColor } from './list-element-per-color';
 import { normalizeQuotedStrings } from './normalize-strings';
 import { readLines } from './read-lines';
-import { splitLines } from './split-lines';
+import { reversedSplitLines, splitLines } from './split-lines';
 
 export const pathToJsonColours_: PathOrFileDescriptor =
   '/home/luxcium/projects/main-POP-N-LOCK-x1DF2/data/extensions/pop-n-lock-theme-vscode/src/components/mappings/JSON/colors-auto.json';
@@ -14,7 +14,7 @@ async function getListElementPerColorObject(
   pathToJsonColours: PathOrFileDescriptor
 ) {
   const step1 = readLines;
-  const step2 = splitLines;
+  const step2 = reversedSplitLines;
   const step3 = extractColorInformation;
   const step4 = normalizeQuotedStrings;
   const step5 = listElementPerColor;
@@ -26,7 +26,7 @@ export async function getColorElementsList(
   includeNullElements: boolean = false
 ) {
   const step1 = readLines;
-  const step2 = splitLines;
+  const step2 = reversedSplitLines;
   const step3 = extractColorInformation;
   const step4 = normalizeQuotedStrings;
   const step5 = createColorElementsList;
@@ -45,11 +45,10 @@ export {
 
 main();
 async function main() {
-  const colorElementList = await getColorElementsList(pathToJsonColours_, true);
+  const colorElementList = await getColorElementsList(pathToJsonColours_);
   colorElementList
-    .filter(item => item.isNull)
+
+    // .filter(item => item.isNull)
     .forEach(item => console.log(item));
   return void 0;
 }
-
-export function reverse() {}
--- src/components/helpers/get-list-element-per-color-object.ts/split-lines.ts
+++ src/components/helpers/get-list-element-per-color-object.ts/split-lines.ts
@@ -7,12 +7,11 @@ export async function splitLines(
     .filter(line => line)
     .filter(line => !re.test(line))
     .map(line => {
-      return line.split(':').reverse() as [string, string];
+      return line.split(':') as [string, string];
     })
     .filter(tuple => tuple[0].trim())
     .filter(tuple => tuple[1].trim());
 }
-// export type ArrayOrTuple<T> = Array<T> | [T, T];
 
 export async function reverseItems<T1, T2>(
   tuples: Promise<[T1, T2][]>
@@ -22,34 +21,3 @@ export async function reverseItems<T1, T2>(
 
 export const reversedSplitLines = async (lines: Promise<string[]>) =>
   reverseItems(splitLines(lines));
-
-// const stringTuple = ['bob', 'alice'];
-// const stringTupleAnotated: [string, string] = ['bob', 'alice'];
-
-// let stringArrayAnotated: Array<string>;
-// stringArrayAnotated = stringTupleAnotated;
-// stringArrayAnotated = stringTuple;
-
-// let tupleAnotated: [string, string];
-// tupleAnotated = stringArrayAnotated;
-// tupleAnotated = stringTuple;
-
-// void stringTuple, tupleAnotated;
-
-// export type ArrayOrTuple2<T, U> = U extends [T, T] ? [T, T] : Array<T>;
-
-export type InTuple = [string, string];
-export type OutTuple = [string, string];
-
-export function reverseItems2<I extends InTuple, O extends OutTuple>(
-  tuples: Promise<I[]>
-): Promise<O[]> {
-  return tuples.then(sets => {
-    return sets.map(set => set.reverse() as O);
-  });
-}
-
-const input = Promise.resolve([['Foo and Bar', 42] as [string, number]]);
-const output = reverseItems(input);
-
-void output;
