a0c343cf845dfb24361a05fe5f69a19ac6686bef
Benjamin Vincent (Luxcium) <luxcium@neb401.com>
2021-08-03 18:26:15 -0400

    implements chroma-hex and implements its testing
    Signed-off-by: Benjamin Vincent (Luxcium) <luxcium@neb401.com>

--- helpers/tools/functions/utils/chroma-hex.ts
+++ helpers/tools/functions/utils/chroma-hex.ts
@@ -1,4 +1,25 @@
 import chroma from 'chroma-js';
 
-export const chromaHex = (colorHex: string) =>
-  chroma(colorHex).hex('rgba').toUpperCase();
+export const chromaHex = (colorHex: string, prefix: string = '#') =>
+  `${prefix}${chroma(colorHex).hex('rgba').toUpperCase().slice(1)}`;
+
+export function templatedChromaHex(
+  template: string | [string] | [string, string] = 'x'
+) {
+  let left = '';
+  let right = '';
+  if (typeof template === 'string') {
+    left = template;
+  }
+
+  if (Array.isArray(template)) {
+    left = template[0] ?? '';
+    right = template[1] ?? '';
+  }
+  return (colorHex: string) => `${chromaHex(colorHex, left)}${right}`;
+}
+
+// function MAIN() {
+//   console.log(chromaHex('FFE1'));
+// }
+// if (require?.main?.filename === __filename) MAIN();
