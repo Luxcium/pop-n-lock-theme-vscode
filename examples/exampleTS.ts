1a82d19f7379e3ff45f56e4726f0f9643b89fc30
Benjamin Vincent Kasapoglu <luxcium@neb401.com>
2022-10-06 16:08:41 -0400

    Update: Examples files for TypeScript
    Signed-off-by: Benjamin Vincent Kasapoglu <luxcium@neb401.com>

--- examples/exampleTS.ts
+++ examples/exampleTS.ts
@@ -158,7 +158,7 @@ export const asyncAnonymArowFunction = async () => {
 function delayedHello(
   name: string,
   delay: number = Delays.Medium
-): Promise<string> {
+): Promise<any> {
   return new Promise((resolve: (value?: string) => void) =>
     setTimeout(() => resolve(`Hello, ${name}`), delay)
   );
@@ -180,7 +180,7 @@ export class ClassName extends ParentClass implements IInterface {
   private _othervalue: any;
   public constructor(parametre: Arguments) {
     super();
-    if (isNaN(parametre.angel) && typeof parametre.value === 'number') {
+    if (isNaN(parametre.angel as any) && typeof parametre.value === 'number') {
       this.angel = Infinity;
     }
   }
@@ -191,6 +191,71 @@ export class ClassName extends ParentClass implements IInterface {
     const angel = checker ? 48 : 47;
     const aNewClass = new ClassName({ value: 'value', angel: angel });
     console.log(aNewClass.value, checker, Math.PI);
+    const myObjeWithClass = {ClassName:ClassName}
+    myObjeWithClass.ClassName.staticValue
+    ClassName.staticValue
+        /*
+
+## {ClassName:ClassName}
+meta.object-literal.key.ts
+meta.object.member.ts
+meta.objectliteral.ts
+meta.var.expr.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+## {...:ClassName}
+variable.other.readwrite.ts
+meta.object.member.ts
+meta.objectliteral.ts
+meta.var.expr.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+## {ClassName}
+variable.other.readwrite.ts
+meta.object.member.ts
+meta.objectliteral.ts
+meta.var.expr.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+
+## myObjeWithClass
+variable.other.object.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+## ClassName
+variable.other.object.property.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+## staticValue
+variable.other.property.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+
+## ClassName (loner)
+variable.other.object.ts
+meta.block.ts
+meta.method.declaration.ts
+meta.class.ts
+source.ts
+    */
+
     const others = [450, null, true, false, undefined];
     return { aNewClass, others };
   }
